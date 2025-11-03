bd08fa0058f80b2a0e487d3f23f092499155a145
CarterC55 <carterchinn55@gmail.com>
2025-11-03 16:36:41 -0500

    Setup the hashtable bucket section of the cpp file, nothing added to them yet aside from the constructors.

--- HashTable.cpp
+++ HashTable.cpp
@@ -11,4 +11,30 @@
 #include <functional>
 #include <random>
 
+//HashTableBucket
+HashTableBucket::HashTableBucket(): key(), value(0), state(BucketType::ESS) {}
+HashTableBucket::HashTableBucket(const std::string& key, const size_t& value): key(key), value(value), state(BucketType::NORMAL) {}
+
+void HashTableBucket::load(const std::string& key, const size_t& value) {}
+
+bool HashTableBucket::isEmpty() const {}
+
+bool HashTableBucket::isEmptySinceStart() const {}
+
+bool HashTableBucket::isEmptyAfterRemove() const {}
+
+void HashTableBucket::makeESS() {}
+void HashTableBucket::makeEAR() {}
+
+const std::string& HashTableBucket::getKey() const {}
+const size_t& HashTableBucket::getValue() const {}
+size_t& HashTableBucket::getValue() {}
+
+BucketType HashTableBucket::getType() const {}
+
+std::ostream& operator<<(std::ostream& os, const HashTableBucket& bucket) {}
+
+//HashTable
+
+
 
